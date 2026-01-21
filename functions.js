86d6468f4a0428a3fd39c0beb4b4844b7c385482
symmagency <monteiroux@gmail.com>
2026-01-21 14:33:10 -0300

    Ajusta

---
+++ functions.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+$('#cabecalho .conteudo-topo .inferior .span8.busca-mobile').after(`
+    <div class="h-actions">
+        <div class="h-user">
+            <img src="https://cdn.awsli.com.br/2923/2923109/arquivos/user.svg" alt="Minha conta">
+            <span>Entrar</span>
+        </div>
+
+        <div class="h-search visible-phone">
+            <img src="https://cdn.awsli.com.br/2923/2923109/arquivos/search.svg" alt="Buscar">
+        </div>
+
+        <div class="h-menu visible-phone">
+            <img src="https://cdn.awsli.com.br/2923/2923109/arquivos/menu.svg" alt="Menu">
+        </div>
+    </div>
+`);
+
+
+if ($(window).width() > 768) {
+//Desktop
+
+
+
+//Fim desktop
+} else {
+//Mobile    
+
+//Fim mobile
+}
+
+});
