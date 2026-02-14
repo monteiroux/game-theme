6f9c7c687582e8f6adeaaca57d31b36217579679
monteiroux <monteiroux@gmail.com>
2026-02-14 12:34:52 -0300

    Ajusta posição do código do compre junto

--- functions.js
+++ functions.js
@@ -337,27 +337,27 @@ if ($(window).width() > 768) {
                 );
             }
         }
+    
+    });
 
-        $('.compre-junto__imagem img').each(function () {
+    $('.compre-junto__imagem img').each(function () {
 
-            var $img = $(this);
-            var src = $img.attr('src');
-        
-            if (!src) return;
-        
-            // troca 150x150 por 300x300
-            var newSrc = src.replace('/150x150/', '/300x300/');
-        
-            if (newSrc !== src) {
-                $img.attr('src', newSrc);
-        
-                // caso exista lazyload
-                if ($img.attr('data-src')) {
-                    $img.attr('data-src', newSrc);
-                }
+        var $img = $(this);
+        var src = $img.attr('src');
+    
+        if (!src) return;
+    
+        // troca 150x150 por 300x300
+        var newSrc = src.replace('/150x150/', '/300x300/');
+    
+        if (newSrc !== src) {
+            $img.attr('src', newSrc);
+    
+            // caso exista lazyload
+            if ($img.attr('data-src')) {
+                $img.attr('data-src', newSrc);
             }
-        
-        });
+        }
     
     });
     
