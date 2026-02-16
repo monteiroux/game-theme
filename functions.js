d1d1e8d2577c4a909e23954eec3f593249d46686
monteiroux <monteiroux@gmail.com>
2026-02-15 23:11:02 -0300

    acoes mobile só no mobile

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 // Ajustes gerais
 
 $('#cabecalho .conteudo-topo .inferior  ').after(`
-    <div class="h-actions">
+    <div class="h-actions visible-phone">
         <a href="/conta/login" class="h-user">
             <img src="https://cdn.awsli.com.br/2923/2923109/arquivos/user.svg" alt="Minha conta">
             <span>Entrar</span>
